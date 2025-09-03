c2978f079753a3b24728dcde30b3eaabe6e432dd
igor.shpetnyi@gmail.com <igor.shpetnyi@gmail.com>
2025-09-03 15:00:31 +0300

    edit proxy

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import requests
 
 app = FastAPI()
 
+NGROK_URL = "https://teaching-annually-asp.ngrok-free.app"
+
 # Псевдо-endpoint проксі
-@app.get("/api/proxy/")
-async def proxy(url: str):
+@app.get("/proxy/")
+async def proxy(data: str):
     # Робимо запит до стороннього сервера
-    r = requests.get(url, headers={"ngrok-skip-browser-warning": "true"}, stream=True)
+    r = requests.get(f"{NGROK_URL}{data}", headers={"ngrok-skip-browser-warning": "true"}, stream=True)
     return Response(content=r.content, media_type=r.headers.get("content-type"))
