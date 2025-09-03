778ea1df41ae70e1174d6e02eeb95fc79011087f
igor.shpetnyi@gmail.com <igor.shpetnyi@gmail.com>
2025-09-03 15:05:32 +0300

    add cors

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI, Request, Response
 import requests
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    # "http://localhost:5173",
+    # "https://igor-shpetnyi.github.io",
+    "*",  # можна для тесту дозволити всім, але небезпечно в продакшені
+]
+
+app.add_middleware(
+            CORSMiddleware,
+            allow_origins=origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 NGROK_URL = "https://teaching-annually-asp.ngrok-free.app"
 
 # Псевдо-endpoint проксі
